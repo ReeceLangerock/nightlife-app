b63c87f8e11f2c062e01c0ad3865b283bfe42bc4
ReeceLangerock <reecelangerock@gmail.com>
2017-02-12 21:11:02 -0600

    correct signout method for cookieSession

--- controllers/logout.js
+++ controllers/logout.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 //sign out of oAuth
-router.get('/', (req, res) => {
-  console.log("signout");
+router.get('/', (req, res) => {  
   req.logout();
-  req.session.destroy();
+  req.session = null;
   res.redirect('/');
 });
 
